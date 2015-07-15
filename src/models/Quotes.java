d97c10a26b1a8b9ae2c3667764c1da1fdc77d686
hafsasfah <hafsa.vahidy@gmail.com>
2015-07-14 22:48:13 -0400

    working with TreeMap

--- src/models/Quotes.java
+++ src/models/Quotes.java
@@ -2,24 +2,50 @@ package models;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Quotes {
 	String quote;
 	String author;
+	TreeMap<String, String> entry = new TreeMap();
+	
+	/*
+	 * Add a Getter returning the key/value pairs
+	   Add a Setter setting the key/value pairs
+	 * Set set = tmap.entrySet();
+      Iterator iterator = set.iterator();
+      while(iterator.hasNext()) {
+         Map.Entry mentry = (Map.Entry)iterator.next();
+         System.out.print("key is: "+ mentry.getKey() + " & Value is: ");
+         System.out.println(mentry.getValue());
+	 */
+	
 	
 	public Quotes() {
 		
 	}
 	
+	public TreeMap getEntry() {
+		return entry;
+	}
+	
+	public void setEntry(String quote, String author) {
+		entry.put(author, quote);
+		
+	}
+
 	public String getQuote() {
 		return quote;
 	}
+	
 	public void setQuote(String quote) {
 		this.quote = quote;
 	}
+	
 	public String getAuthor() {
 		return author;
 	}
+	
 	public void setAuthor(String author) {
 		this.author = author;
 	}
